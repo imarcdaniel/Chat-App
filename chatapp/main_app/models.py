7662e83dc035935a7886063e57e9620cca7103bd
imarcdaniel <LORENTZDANIEL@Pegasus.local>
2021-10-27 17:39:31 -0400

    sub commit to pull a push from colleague

--- chatapp/main_app/models.py
+++ chatapp/main_app/models.py
@@ -4,7 +4,16 @@ from datetime import date
 
 # Import the User
 from django.contrib.auth.models import User
+from django.db.models.deletion import SET_NULL
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.CharField(max_length=150, default=SET_NULL)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Message(models.Model):
@@ -22,6 +31,17 @@ class Channel(models.Model):
     creationdate = models.DateField(auto_now_add=True)
     messages = models.ForeignKey(
         Message, blank=True, null=True, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class Friend(models.Model):
+    users = models.ManyToManyField(UserProfile)
+    current_user = models.ForeignKey(
+        UserProfile, related_name="owner", null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.current_user}"
