989eb85b574630b2f31f87d5716cb5ce6d914d12
Eric Chan <ericmbc@hotmail.com>
2021-10-27 10:16:59 -0400

    websocket

--- chatapp/main_app/models.py
+++ chatapp/main_app/models.py
@@ -8,3 +8,6 @@ from django.contrib.auth.models import User
 class Message(models.Model):
    body = models.CharField(max_length=150)
    date = models.DateField(auto_now_add=True)
+
+def __str__(self):
+    return self.body
